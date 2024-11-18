65e79e7a7209047e4a5c20a89ddc1fa42b750ec3
naumanmustafa16 <naumanmustafa16@gmail.com>
2024-11-18 11:28:05 +0900

    Add page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def current_date
+      Time.now.strftime('%B %d, %Y') # Example: "November 18, 2024"
+    end
+
+    def current_time
+      Time.now.strftime('%I:%M %p') # Example: "03:45 PM"
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
     }
+  validates :title, length: {minimum:10, message: "Be smart enough to add a title of at leaste 10 characters"}
 end
